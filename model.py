30d8edfb96863d88cf484b4ba939825fb4ced170
yamaru12345 <65715903+yamaru12345@users.noreply.github.com>
2020-07-03 21:15:55 +0900

    Update model.py

--- model.py
+++ model.py
@@ -4,8 +4,8 @@ from torchvision import models
 
 def load_model(model, out_features):
     net = None
-    if model == 'VGG19':
-        net = models.vgg19_bn(pretrained=True)
+    if model == 'VGG16':
+        net = models.vgg16(pretrained=True)
         net.classifier[6] = nn.Linear(in_features=4096, out_features=out_features)
         for name, param in net.named_parameters():
             if name in ['classifier.6.weight', 'classifier.6.bias']:
